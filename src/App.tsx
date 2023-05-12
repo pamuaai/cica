d18439d9f304b3bc39b739bf7a41df568adb31db
markbpaines <mark.paines@bankmonitor.hu>
2023-05-12 20:15:58 +0200

    Changed to tsx and scss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import "./App.scss";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello Cica!</h1>
+      <h2>Van gazdád?</h2>
+    </div>
+  );
+};
+
+export default App;
