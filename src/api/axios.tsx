911464337fda9eb85211bce48ea7ecc46cf92f8d
markbpaines <mark.paines@bankmonitor.hu>
2023-05-29 17:47:53 +0200

    trying out NODE_ENV

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:4000";
-const BASE_URL = "http://cica.profroxas.duckdns.org";
+// const BASE_URL = "http://cica.profroxas.duckdns.org";
+const BASE_URL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:4000/"
+    : "http://cica.profroxas.duckdns.org";
 export default axios.create({
   baseURL: BASE_URL,
 });
